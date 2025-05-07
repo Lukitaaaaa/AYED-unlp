bb35991ad3900eea22043becb8f1cf75732a2364
Lukitaaaaa <lucasomargallardo@gmail.com>
2025-05-06 21:24:08 -0300

    practica 3 ejercicio 2

--- tp3/ejercicio2/RecorridosAG.java
+++ tp3/ejercicio2/RecorridosAG.java
@@ -1,43 +1,92 @@
 package tp3.ejercicio2;
 
 import tp3.ejercicio1.*;
+
 import java.util.List;
 import java.util.LinkedList;
+import tp1.ejercicio8.Queue;
+
 
 public class RecorridosAG {
 
 	
 	public List<Integer> numerosImparesMayoresQuePreOrden (GeneralTree <Integer> a,Integer n){
 		List<Integer> aux = new LinkedList<Integer>();
-		return this.numerosImparesMayoresQuePreOrden(a, aux, n);
+		this.numerosImparesMayoresQuePreOrden(a, aux, n);	
+		return aux;
 	}
 	
-	private List<Integer> numerosImparesMayoresQuePreOrden(GeneralTree <Integer> a, List<Integer> aux, Integer n){
+	private void numerosImparesMayoresQuePreOrden(GeneralTree <Integer> a, List<Integer> aux, Integer n){
 		if((a.getData()>n) && (a.getData()%2 != 0)) {
 			aux.add(a.getData());
 			System.out.print(a.getData()+" ");
 		}
 		List<GeneralTree<Integer>> children = a.getChildren();
 		for (GeneralTree<Integer> child: children) {	
-			aux = this.numerosImparesMayoresQuePreOrden(child, aux, n);
+			this.numerosImparesMayoresQuePreOrden(child, aux, n);
 		}
-		return aux;
 			 
 	}
 	
 	public List<Integer> numerosImparesMayoresQueInOrden (GeneralTree <Integer> a,Integer n){
 		List<Integer> aux = new LinkedList<Integer>();
+		this.numerosImparesMayoresQueInOrden(a, aux, n);
 		return aux;
+	}
+	
+	private void numerosImparesMayoresQueInOrden(GeneralTree <Integer> a, List<Integer> aux,Integer n){
+		if(a.hasChildren())
+			this.numerosImparesMayoresQueInOrden(a.getChildren().getFirst(), aux, n);
+		
+		if((a.getData()>n) && (a.getData()%2 != 0)) {
+			aux.add(a.getData());
+			System.out.print(a.getData()+" ");
+		}
+		
+		for(int i=1; i< a.getChildren().size(); i++)
+            numerosImparesMayoresQueInOrden(a.getChildren().get(i),aux,n);
+		
 	}
 	
 	public List<Integer> numerosImparesMayoresQuePostOrden (GeneralTree <Integer> a,Integer n){
 		List<Integer> aux = new LinkedList<Integer>();
+		this.numerosImparesMayoresQuePostOrden(a, aux, n);
 		return aux;
+	}
+	
+	private void numerosImparesMayoresQuePostOrden(GeneralTree <Integer> a, List<Integer> aux,Integer n){
+		List<GeneralTree<Integer>> children = a.getChildren();
+		for (GeneralTree<Integer> child: children)
+			this.numerosImparesMayoresQueInOrden(child, aux, n);
+		
+		if((a.getData()>n) && (a.getData()%2 != 0)) {
+			aux.add(a.getData());
+			System.out.print(a.getData()+" - ");
+		}
 	}
 	
 	public List<Integer> numerosImparesMayoresQuePorNiveles (GeneralTree <Integer> a,Integer n){
 		List<Integer> aux = new LinkedList<Integer>();
+		this.numerosImparesMayoresQuePorNiveles(a, aux, n);
 		return aux;
+	}
+	
+	private void numerosImparesMayoresQuePorNiveles (GeneralTree <Integer> a, List<Integer> aux,Integer n){
+		GeneralTree<Integer> tree_aux;
+		Queue<GeneralTree <Integer>> queue = new Queue<GeneralTree <Integer>>();
+		queue.enqueue(a);
+		
+		while (!queue.isEmpty()) {
+			tree_aux = queue.dequeue();
+			if((tree_aux.getData()>n) && (tree_aux.getData()%2 != 0)) {
+				aux.add(tree_aux.getData());
+				System.out.print(tree_aux.getData()+" ");
+			}
+			List<GeneralTree<Integer>> children = tree_aux.getChildren();
+			for (GeneralTree<Integer> child: children) {
+				queue.enqueue(child);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -60,6 +109,12 @@ public class RecorridosAG {
 		
 		RecorridosAG r = new RecorridosAG();
 		r.numerosImparesMayoresQuePreOrden(a, 3);
+		System.out.println();
+		r.numerosImparesMayoresQueInOrden(a, 3);
+		System.out.println();
+		r.numerosImparesMayoresQuePostOrden(a, 3);
+		System.out.println();
+		r.numerosImparesMayoresQuePorNiveles(a, 3);
 	}
 	
 }
--- tp3/ejercicio3/GeneralTree.java
+++ tp3/ejercicio3/GeneralTree.java
@@ -2,6 +2,7 @@ package tp3.ejercicio3;
 
 import java.util.LinkedList;
 import java.util.List;
+import tp1.ejercicio8.Queue;
 
 public class GeneralTree<T>{
 
@@ -57,17 +58,67 @@ public class GeneralTree<T>{
 			children.remove(child);
 	}
 	
-	public int altura() {	 
-			
-		return 0;
+	public int altura() {
+		int aux = 0;
+		int hijos = 0;
+		if(this.isEmpty()) {
+			return aux;
+		}
+		
+		if(this.hasChildren())
+			aux++;
+			List<GeneralTree<T>> children = this.getChildren();
+			for (GeneralTree<T> child: children) {	
+				hijos = child.altura();
+			}
+		return aux + hijos;
 	}
 	
 	public int nivel(T dato){
-		return 0;
-	  }
+		int aux = 0; 
+		boolean esta = false;
+		GeneralTree<T> tree_aux;
+		Queue<GeneralTree<T>> queue = new Queue<GeneralTree<T>>();
+		queue.enqueue(this);
+		while (!queue.isEmpty() || !esta) {
+			aux++;
+			tree_aux = queue.dequeue();
+			if(tree_aux.getData() == dato)
+				esta = true;
+			List<GeneralTree<T>> children = tree_aux.getChildren();
+			for (GeneralTree<T> child: children) {
+				queue.enqueue(child);
+			}
+		}
+		return aux;
+	}
 
 	public int ancho(){
 		
 		return 0;
+	}
+	
+	public static void main(String[] args) {
+		GeneralTree<Integer> a1 = new GeneralTree<Integer>(1);
+		List<GeneralTree<Integer>> children2 = new LinkedList<GeneralTree<Integer>>();
+		children2.add(new GeneralTree<Integer>(21));
+		children2.add(new GeneralTree<Integer>(22));
+		children2.add(new GeneralTree<Integer>(23));
+		
+		GeneralTree<Integer> a2 = new GeneralTree<Integer>(2, children2);
+		List<GeneralTree<Integer>> children3 = new LinkedList<GeneralTree<Integer>>();
+		children3.add(new GeneralTree<Integer>(31));
+		children3.add(new GeneralTree<Integer>(32));
+		
+		GeneralTree<Integer> a3 = new GeneralTree<Integer>(3, children3);
+		List<GeneralTree<Integer>> childen = new LinkedList<GeneralTree<Integer>>();
+		childen.add(a1);childen.add(a2);childen.add(a3);
+		
+		GeneralTree<Integer> a = new GeneralTree<Integer>(0, childen);
+		
+		System.out.println("Altura: "+a.altura());
+		System.out.println("Nivel: "+a.nivel(31));
+		//System.out.println("Altura: "+a.altura());
+		
 	}
 }
